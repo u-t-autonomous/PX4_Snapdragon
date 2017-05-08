be16b74dddc8fba17b135849aede6288d461b23b
Beat Küng <beat-kueng@gmx.net>
2017-05-08 11:47:22 +0200

    refactor navio_sysfs_pwm_out: rename to rpi_pwm_out

--- cmake/configs/posix_rpi_common.cmake
+++ cmake/configs/posix_rpi_common.cmake
@@ -74,7 +74,7 @@ set(config_module_list
 	#
 	drivers/gps
 	drivers/navio_sysfs_rc_in
-	drivers/navio_sysfs_pwm_out
+	drivers/rpi_pwm_out
 	drivers/navio_gpio
 	drivers/navio_rgbled
 	drivers/pwm_out_sim

virenbajaj/Firmware

add new navigator_item struct (will replace mission_item)

src/modules/navigator/mission.cpp
@@ -512,7 +512,9 @@ Mission::set_mission_items()
 
 	/*********************************** handle mission item *********************************************/
 
-	// convert mission item to local navigation item
+	// convert mission_item to local navigation_item
+	//TODO use _navigation_item instead of _mission_item
+	_navigator->mission_item_to_navigation_item(&_navigation_item, &_mission_item);
 	_navigator->global_to_local(&_mission_item);
 
 	/* handle position mission items */

src/modules/navigator/mission_block.h
@@ -130,6 +130,7 @@ protected:
 	float get_time_inside(const struct mission_item_s &item);
 
 	mission_item_s _mission_item{};
+	navigator_item_s _navigation_item{};
 
 	bool _waypoint_position_reached{false};
 	bool _waypoint_yaw_reached{false};

src/modules/navigator/navigator.h
@@ -235,6 +235,8 @@ public:
 
 	void 		global_to_local(struct mission_item_s *item);
 
+	void 		mission_item_to_navigation_item(struct navigator_item_s *nav_item, struct mission_item_s *mission_item);
+
 private:
 
 	bool		_task_should_exit{false};	/**< if true, sensor task should exit */

src/modules/navigator/navigator_main.cpp
@@ -866,6 +866,36 @@ Navigator::global_to_local(struct mission_item_s *item)
 }
 
 void
+Navigator::mission_item_to_navigation_item(struct navigator_item_s *nav_item, struct mission_item_s *mission_item)
+{
+
+	map_projection_project(get_local_reference_pos(), mission_item->lat, mission_item->lon, &nav_item->x, &nav_item->y);
+
+	if (mission_item->altitude_is_relative) {
+		nav_item->z = - mission_item->altitude;
+
+	} else {
+		nav_item->z = - (mission_item->altitude - get_local_reference_alt());
+	}
+
+	nav_item->yaw = mission_item->yaw;
+	nav_item->acceptance_radius = mission_item->acceptance_radius;
+	nav_item->loiter_radius = mission_item->loiter_radius;
+	nav_item->nav_cmd = mission_item->nav_cmd;
+	nav_item->do_jump_mission_index = mission_item->do_jump_mission_index;
+	nav_item->do_jump_repeat_count = mission_item->do_jump_repeat_count;
+	nav_item->do_jump_current_count = mission_item->do_jump_current_count;
+	nav_item->frame = mission_item->frame;
+	nav_item->origin = mission_item->origin;
+	nav_item->loiter_exit_xtrack = mission_item->loiter_exit_xtrack;
+	nav_item->force_heading = mission_item->force_heading;
+	nav_item->autocontinue = mission_item->autocontinue;
+	nav_item->disable_mc_yaw = mission_item->disable_mc_yaw;
+	nav_item->vtol_back_transition = mission_item->vtol_back_transition;
+
+}
+
+void
 Navigator::add_fence_point(int argc, char *argv[])
 {
 	_geofence.addPoint(argc, argv);

u-t-autonomous/PX4_Snapdragon

src/modules/navigator/mission.cpp
@@ -512,7 +512,9 @@ Mission::set_mission_items()
 
 	/*********************************** handle mission item *********************************************/
 
-	// convert mission item to local navigation item
+	// convert mission_item to local navigator_item
+	//TODO use _navigator_item instead of _mission_item
+	_navigator->mission_item_to_navigator_item(&_navigator_item, &_mission_item);
 	_navigator->global_to_local(&_mission_item);
 
 	/* handle position mission items */

src/modules/navigator/mission_block.h
@@ -130,6 +130,7 @@ protected:
 	float get_time_inside(const struct mission_item_s &item);
 
 	mission_item_s _mission_item{};
+	navigator_item_s _navigator_item{};
 
 	bool _waypoint_position_reached{false};
 	bool _waypoint_yaw_reached{false};

src/modules/navigator/navigator.h
@@ -235,6 +235,8 @@ public:
 
 	void 		global_to_local(struct mission_item_s *item);
 
+	void 		mission_item_to_navigator_item(struct navigator_item_s *nav_item, struct mission_item_s *mission_item);
+
 private:
 
 	bool		_task_should_exit{false};	/**< if true, sensor task should exit */

src/modules/navigator/navigator_main.cpp
@@ -866,6 +866,36 @@ Navigator::global_to_local(struct mission_item_s *item)
 }
 
 void
+Navigator::mission_item_to_navigator_item(struct navigator_item_s *nav_item, struct mission_item_s *mission_item)
+{
+
+	map_projection_project(get_local_reference_pos(), mission_item->lat, mission_item->lon, &nav_item->x, &nav_item->y);
+
+	if (mission_item->altitude_is_relative) {
+		nav_item->z = - mission_item->altitude;
+
+	} else {
+		nav_item->z = - (mission_item->altitude - get_local_reference_alt());
+	}
+
+	nav_item->yaw = mission_item->yaw;
+	nav_item->acceptance_radius = mission_item->acceptance_radius;
+	nav_item->loiter_radius = mission_item->loiter_radius;
+	nav_item->nav_cmd = mission_item->nav_cmd;
+	nav_item->do_jump_mission_index = mission_item->do_jump_mission_index;
+	nav_item->do_jump_repeat_count = mission_item->do_jump_repeat_count;
+	nav_item->do_jump_current_count = mission_item->do_jump_current_count;
+	nav_item->frame = mission_item->frame;
+	nav_item->origin = mission_item->origin;
+	nav_item->loiter_exit_xtrack = mission_item->loiter_exit_xtrack;
+	nav_item->force_heading = mission_item->force_heading;
+	nav_item->autocontinue = mission_item->autocontinue;
+	nav_item->disable_mc_yaw = mission_item->disable_mc_yaw;
+	nav_item->vtol_back_transition = mission_item->vtol_back_transition;
+
+}
+
+void
 Navigator::add_fence_point(int argc, char *argv[])
 {
 	_geofence.addPoint(argc, argv);